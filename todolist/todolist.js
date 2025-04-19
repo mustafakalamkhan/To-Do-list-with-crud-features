0a8bc0279ff73575535f6c934e4126bc893eccd5
mustafakalamkhan <mustafaakalamkhan@gmail.com>
2025-04-20 00:38:09 +0530

    commit2

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -1,4 +1,7 @@
+console.log("todolist.js loaded");
 const inputBox = document.getElementById("input-box");
+const dueDate = document.getElementById("due-date");
+const reminderTime = document.getElementById("reminder-time");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
@@ -6,21 +9,47 @@ function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
     } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        var li = document.createElement("li"); 
+        var taskText = document.createElement('strong');
+        taskText.innerHTML = inputBox.value; // test123
+        if (dueDate.value)
+        taskText.innerHTML += '<br>'+`Due Date: ${dueDate.value}`; 
+        if(reminderTime.value) {
+            scheduleReminder(inputBox.value, reminderTime.value); // Fixed "schedulereminder" to "scheduleReminder"
+            taskText.innerHTML += '<br>'+`Reminder : ${reminderTime.value}`
+          }
+        console.log('tasktext == ',taskText);
+        li.appendChild(taskText);
         listContainer.appendChild(li);
-        
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
-    inputBox.value = "";
+    inputBox.value = "";
+    dueDate.value = "";
+    reminderTime.value = "";
     saveData();
 }
 
+function scheduleReminder(task, time) { // Fixed function name to match call
+    const now = new Date();
+    const reminder = new Date();
+    const [hours, minutes] = time.split(":");
+
+    reminder.setHours(hours);
+    reminder.setMinutes(minutes);
+    reminder.setSeconds(0);
+
+    const timeDiff = reminder.getTime() - now.getTime();
+
+    if (timeDiff > 0) 
+        setTimeout(() => { alert(`Reminder: ${task}`); }, timeDiff);
+    }
+
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === 'LI'){
-        e.target.classLift.toggle("checked");
+        e.target.classList.toggle("checked"); // Fixed "classLift" to "classList"
         saveData();
     }
     else if(e.target.tagName === "SPAN"){
